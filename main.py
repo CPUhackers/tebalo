6ec62e7175bb8323a0d32a4895f2f9a0de438753
Vishnu Ks <hackerkid@vishnuks.com>
2016-12-01 03:16:09 +0530

    Remove print statements

--- main.py
+++ main.py
@@ -16,13 +16,9 @@ def expected_set_frequencies():
 	block_size = (max_load - min_load)/float(no_of_avail_freqs)
 	set_freq = []
 	current_loads = utils.get_current_loads()
-	#print "current loads "
-	#print current_loads
 	for load in current_loads:
 		temp = min_load
 		for freq in reversed(avail_freqs):
-			#print temp 
-			#print temp + block_size 
 			if(temp <= load  <= (temp + block_size)) :
 				set_freq.append(freq)
 				break
@@ -44,7 +40,6 @@ def reduce_level(freq):
     	if freq == avail_freqs[i] :
     		return avail_freqs[i+1]
 
-    #never comes here		
     return avail_freqs[no_of_avail_freqs/2]			
 
 def set_frequencies():
@@ -52,27 +47,19 @@ def set_frequencies():
 
 	while True:
 		freqs = expected_set_frequencies()
-		#print "freqs"
-		print freqs
 		for i in range(0,core_count) :
 			freq = freqs[i]
-			print current_temperature(i) + " : temps : " + str(safe_temperature)
 			if utils.get_current_battery() < 10 :
 				min_freq = avail_freqs[-1]
 				utils.set_core_frequency(i,min_freq)					
 			elif int(current_temperature(i)) <= safe_temperature:
-				#print "hey here" + freq
 				utils.set_core_frequency(i,freq)	
 			else :
-				print "here again"
 				curr_freq = utils.get_current_frequency(i)
 				previous_level_freq = reduce_level(curr_freq)
-				#print i 
-				#print  "th core freq: " + curr_freq+ " hey: " + previous_level_freq
 				utils.set_core_frequency(i,previous_level_freq)	
 		time.sleep(1)    	
 
 if __name__ == "__main__":
-	#freqs = expected_set_frequencies()
 	set_frequencies()
 	
